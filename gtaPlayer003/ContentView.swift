3ffb731767859be33f914c2a54418f0ee2f39332
zaycation <contact@zaycation.dev>
2020-01-16 22:47:34 -0800

    Initial Commit

---
+++ gtaPlayer003/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  gtaPlayer003
+//
+//  Created by Isaiah Thomas on 1/16/20.
+//  Copyright © 2020 zaycation. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
